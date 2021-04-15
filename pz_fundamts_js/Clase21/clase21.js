78defb879fe4cd66371d88813a0f913bafb847b3
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-04-15 11:22:03 -0500

    Clase21= Prototypes p.2

--- pz_fundamts_js/Clase21/clase21.js
+++ pz_fundamts_js/Clase21/clase21.js
@@ -1,15 +1,27 @@
-function Personas(nombre, apellido) {
+function Personas(nombre, apellido, alturaMts) {
     this.nombre = nombre;
     this.apellido = apellido;
+	this.altura = alturaMts
 }
 
 Personas.prototype.saludar = function (){
     console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+	console.log(`Soy una persona ${this.miAltura()}`)
 }
 
-var victor = new Personas('Victor', 'Briseno');
-var Ericka = new Personas('Ericka', 'Luna');
-var Arturo = new Personas('Arturo', 'Martinez');
+const ALTURA_ALTA = 1.8;
+
+Personas.prototype.miAltura = function () {
+	if (this.altura > ALTURA_ALTA){
+		return "Alta"
+	} else {
+		return "Baja"
+	}
+}
+
+var victor = new Personas('Victor', 'Briseno', 1.72);
+var Ericka = new Personas('Ericka', 'Luna', 1.60);
+var Arturo = new Personas('Arturo', 'Martinez', 1.82);
 
 
 
