3d630340c5b703c8c7505139385cb48263a9ebd4
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-04-19 22:47:27 -0500

    Clase34= Multiples Respuestas en Paralelo

---
+++ pz_fundamts_js/Clase30,31,32,33,34/clase30,31,32,33,34.js
@@ -0,0 +1,31 @@
+const API_URL = 'https://swapi.dev/api/';
+
+const PEOPLE_URL = 'people/:id';
+
+const opts = { crossDomain : true };
+
+
+function obtenerPersonaje(id) {
+   return new Promise( (resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+
+        $.get(url,opts, function(data){
+                resolve(data)
+            }).fail(() => reject(id)) ;
+   })
+}
+
+const onError = id => console.log(`Error al concegir personaje  ${id}`)
+
+
+
+var ids = [1,2,3,4,5,6,7]
+
+var promesas = ids.map( id => obtenerPersonaje(id))
+
+Promise
+    .all(promesas)
+    .then(personajes => console.table(personajes))
+    .catch(onError)
+
+
