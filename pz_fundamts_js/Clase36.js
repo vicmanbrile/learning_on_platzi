9388fe86cb70fe0c147de51279584c78bdee453d
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-04-27 22:11:33 -0500

    Clase36= Memoria

---
+++ pz_fundamts_js/Clase36.js
@@ -0,0 +1,18 @@
+function factorial(n){
+
+    if(!this.cache){
+        this.cache = {}
+    }
+
+    if (this.cache[n]){
+        return this.cache[n]
+    }
+
+    if (n === 1) {
+        return 1
+    }
+    this.cache[n] = n * factorial(n - 1)
+    debugger
+    return this.cache[n];
+}
+
