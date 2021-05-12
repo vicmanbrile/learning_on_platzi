1daa49b039a719b9458af1c5e35587ee7787fae2
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-05-11 23:14:17 -0500

    implemento en ES7

---
+++ pz_ecmascript/es7/main.js
@@ -0,0 +1,16 @@
+// Includes --->
+let numbers = [1,2,3,4,5,6,7,8]
+
+if (numbers.includes(7)){
+    console.log('Se encuentra el valor')
+} else {
+    console.log('No se encuentra el valor')
+}
+
+// Elevacion a ala potencia --->
+let base = 4
+let exponente = 2
+
+let resultado = base ** exponente
+
+console.log(resultado)
