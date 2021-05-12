b2c523fcd8d9c73741f376cf3ba0df4ad34984ec
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-05-11 23:40:07 -0500

    Async Await

--- pz_ecmascript/es8/main.js
+++ pz_ecmascript/es8/main.js
@@ -16,3 +16,29 @@ console.log(values)
 const string = 'Hello'
 console.log(string.padStart(7, 'hi'))
 console.log(string.padEnd(12, ' -----'))
+
+// Async Await --->
+const HelloWorld = () => {
+    return new Promise((resolve, reject ) => {
+        (true) ? setTimeout (() => resolve('Hello World'), 3000) : reject( new Error('Test Error'))
+    })
+}
+
+const helloAsync = async () => {
+    let hello = await HelloWorld()
+    console.log(hello)
+}
+
+helloAsync()
+
+// Propuesta para hacerlo correctamente
+const otraFunction = async () => {
+    try {
+        let hello = await HelloWorld()
+        console.log(hello)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+otraFunction()
