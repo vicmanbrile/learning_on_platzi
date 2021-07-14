e971b46e34d01bbd4329e8f4231c8886cf8ee45d
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-07-13 21:13:53 -0500

    add en pantalla

--- pz_profecional_js/assets/MediaPlayer.ts
+++ pz_profecional_js/assets/MediaPlayer.ts
@@ -1,10 +1,11 @@
 class MediaPlayer {
     media: HTMLMediaElement
     plugins: Array<any>
+    container: HTMLElement
     constructor(config) {
         this.media = config.el
         this.plugins = config.plugins || []
-
+        this.initPlayer()
         this.initPlugins()
     }
     private initPlugins() {
@@ -36,6 +37,14 @@ class MediaPlayer {
         } else {
             this.pause()
         }
+    }
+
+    initPlayer() {
+        this.container = document.createElement('div')
+        this.container.style.position = 'relative'
+
+        this.media.parentNode.insertBefore(this.container, this.media)
+        this.container.appendChild(this.media)
     }
 }
 
