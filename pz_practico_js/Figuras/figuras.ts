e996799a0deaa0f8b523a2b4d12ac2f4059244e3
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-07-15 19:57:18 -0500

    const hechas a funciones

--- pz_practico_js/Figuras/figuras.ts
+++ pz_practico_js/Figuras/figuras.ts
@@ -1,51 +1,138 @@
-//Cpodigo del cuadrado
+//Codigo del cuadrado
+class Cuadrado {
+    lado: number
+
+    constructor(lado : number){
+        this.lado = lado
+    }
+    mide(){
+        return `Los lados del cuadrado miden: ${this.lado} cms`
+    }
+    perimetro(){
+        let perimetro = this.lado * 4;
+        return `El perimetro del cuadrado: ${perimetro} cms^2 `
+    }
+    area(){
+        let area = this.lado * this.lado;
+        return `La area del cuadrado: ${area} cms^3`
+    }
+}
 console.group("Codigo del Cuadrado")
-const ladaCuadrado = 5
-console.log(`Los lados del cuadrado miden: ${ladaCuadrado} cm`)
 
-const perimetroCuadrado = ladaCuadrado * 4
-console.log(`El perimetro del cuadrado: ${perimetroCuadrado} cm^2 `)
+const miCuadrado = new Cuadrado(5);
 
-const areaCuadrado = ladaCuadrado * ladaCuadrado
-console.log(`La area del cuadrado: ${areaCuadrado} cm^3`)
+console.log(miCuadrado.mide());
+console.log(miCuadrado.perimetro());
+console.log(miCuadrado.area());
 
 console.groupEnd()
+
+
 //Codigo del triangulo
+
+interface LadosTriangulo {
+    lado_a: number,
+    lado_b: number,
+    lado_c_base: number,
+    altura: number,
+}
+
+class Triangulo {
+    lado_a : number
+    lado_b : number
+    lado_c_base: number
+    altura: number
+
+    constructor(lados: LadosTriangulo){
+        this.lado_a = lados.lado_a
+        this.lado_b = lados.lado_b
+        this.lado_c_base = lados.lado_c_base
+        this.altura = lados.altura
+    }
+
+    ladosTriangulo () {
+        return `Los lados del triángulo miden: ${this.lado_a} cms, ${this.lado_b} cms, ${this.lado_c_base} cms.`
+    }
+
+    perimetro(){
+        let perimetro = this.lado_a + this.lado_b + this.lado_c_base
+        return `El perimetro del triangulo: ${perimetro} cms^2  `
+    }
+
+    area(){
+        let area = this.lado_c_base * this.altura
+        return `La area del triangulo: ${area} cms^3`
+    }
+}
+
 console.group("Codigo del triangulo")
-const ladoTriangulo1 = 6
-const ladoTriangulo2 = 6
-const baseTriangulo = 4
 
-const alturaTriangulo2 = 5.5
+const medidasTriangulo = {
+    lado_a: 6,
+    lado_b: 6,
+    lado_c_base: 4,
+    altura: 5.5,
+}
 
-console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " cms, " + ladoTriangulo2 + " cms, " + baseTriangulo + "cms.");
+const miTriangulo = new Triangulo(medidasTriangulo)
 
-console.log(`La altura del triangulo mide: ${alturaTriangulo2} cm`)
+console.log(miTriangulo.ladosTriangulo())
+console.log(miTriangulo.perimetro())
+console.log(miTriangulo.area())
 
-
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo
-console.log(`El perimetro del triangulo: ${perimetroTriangulo} cm^2 `)
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo2) / 2
-console.log(`La area del triangulo: ${areaTriangulo} cm^3`)
 
 console.groupEnd()
-
 //Código del Circulos
+
+interface Circulos{
+    radio: number
+}
+
+class Circulo{
+    rad: number
+    PI: number
+    constructor(circulo : Circulos){
+        this.rad = circulo.radio
+        this.PI = Math.PI
+    }
+
+    diametro(){
+        let diametro = this.rad * 2
+        return diametro
+    }
+
+    radio(){
+        return `El radio del circulo es ${this.rad} cms.`
+    }
+
+    diametro_verbal(){
+        return `El diametro del circulo es ${this.diametro()} cms.`
+    }
+
+    perimetro(){
+        let perimetro = this.diametro() * this.PI
+        return `El perimetro del circulo es ${perimetro.toFixed(2)} cms.`
+    }
+
+    area(){
+        let area = this.PI * (this.rad ** 2)
+        return `El area del circulo es ${area.toFixed(2)} cms.`
+    }
+
+}
+
 console.group("Circulos");
-//Radio
-const radioCirculo = 4;
-console.log("El radio del circulo es " + radioCirculo + " cms.");
-//Diametro
-const diametroCirculo = radioCirculo * 2;
-console.log("El diametro del circulo es " + diametroCirculo + " cms.");
-// PI
-const PI = Math.PI;
-console.log("PI es " + PI + ".");
-// Perimetro
-const perimetroCirculo = diametroCirculo * PI;
-console.log("El perimetro del circulo es " + perimetroCirculo + " cms.");
-//Area
-const areaCirculo = PI * (radioCirculo**2);
-console.log("El area del circulo es " + areaCirculo + " cms.");
+
+const medidasCirculo = {
+    radio: 4
+}
+
+const miCirculo = new Circulo(medidasCirculo)
+
+console.log(miCirculo.radio())
+console.log(miCirculo.diametro_verbal())
+console.log(miCirculo.perimetro())
+console.log(miCirculo.area())
+
+
 console.groupEnd();
