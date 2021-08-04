7aef9fd7366e64ef9607a9e76bd7e3a2eaed34c9
vicmanbrile <victor.briseno.leon@hotmail.com.mx>
2021-08-03 23:54:53 -0500

    class aplicacionDescuentos

--- pz_practico_js/Descuentos/main.js
+++ pz_practico_js/Descuentos/main.js
@@ -1,8 +1,62 @@
-function restarDescuento(precio, descuento){
-    let desc_decimal = descuento / 100 
-    let precio_desc = 1 - desc_decimal
+const _cupones = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    }
+]
 
-    return precio * precio_desc
+class aplicacionDescuentos{
+    constructor(inputs){
+        this.precio = inputs.precio
+        this.descuento = inputs.descuento
+        this.cupon = inputs.cupon
+        this.validacionCupones()
+        this.restarDescuento()
+    }
+
+    validacionCupones() {
+
+        _cupones.find(() => {
+            _cupones.name === this.cupon
+        })
+
+        if (this.cupon === _cupones.name) {
+            this.userCoupon = true
+        } else {
+            this.userCoupon = false
+        }
+    }
+
+    restarDescuento(){
+        let desc_decimal = this.descuento / 100 
+        let precio_desc = 1 - desc_decimal
+    
+        this.total = this.precio * precio_desc
+    }
+
+    
+
+    printResultado(){
+        document.getElementById('Resultado_').innerText = this.total
+    }
 }
 
-console.log(restarDescuento(120, 15))
+function calculadoraDesc(){
+    let inputs = {
+        precio: Number(document.getElementById('Precio_').value),
+        descuento: Number(document.getElementById('Descuento_').value),
+        cupon: document.getElementById('Descuento_cupon').value
+    }
+
+    let calculo = new aplicacionDescuentos(inputs)
+
+    calculo.printResultado()
+}
