1d5ecde1b5181daeaefa234a9a4635b134bc45af
vicmanbrile <victor.briseno.leon@hotmail.com.mx>
2021-08-06 00:10:16 -0500

    Promedio y Mediana

---
+++ pz_practico_js/promedio_moda_mediana/main.js
@@ -0,0 +1,47 @@
+    /* 
+
+        Calculado la Media Aritmetica --->
+
+    */
+function calcularMediaAritmetica(list){
+    reducionSumar = (accumulator, currentValue) => {
+        return accumulator + currentValue
+    }
+
+    const listaSumada = list.reduce(reducionSumar)
+
+    return listaSumada / list.length
+}
+    /*
+
+        Calculando la Mediana Aritmetica --->
+
+    */
+class calculadoraMediana{
+    constructor(list){
+        this.list = list.sort((a,b) => a - b)
+        this.mitad = this.verificarPar(this.list.length)
+        this.calcularMediana()
+    }
+
+    verificarPar(numero){
+        if(numero % 0 !== 0) { return  true } else { return false}  
+    }
+
+    calcularMediana(){
+        let list_mediana = parseInt(this.list.length / 2)
+
+        if(this.mitad){
+            let element_1 = this.list[list_mediana -1]
+            let element_2 = this.list[list_mediana]
+                
+            this.mediana = calcularMediaAritmetica([element_1, element_2])
+        } else {
+            this.mediana = this.list[list_mediana]
+        }
+    }
+
+    resultado(){
+        return this.mediana
+    }
+}
