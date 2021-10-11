6e246d73cc046a6954e7b1a8959fedb9dbddda17
vicmanbrile <victor.briseno.leon@hotmail.com.mx>
2021-10-10 22:15:16 -0500

    webpack configuracion de desarrollo

--- pz_webpack/webpack.config.dev.js
+++ pz_webpack/webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const TercerPlugin = require('terser-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin') 
 
 
 module.exports = {
@@ -15,7 +12,7 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]' 
     },
-    mode: 'production',
+    mode: 'development',
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -76,14 +73,6 @@ module.exports = {
                 }
             ]
         }),
-        new Dotenv(),
-        new CleanWebpackPlugin()
+        new Dotenv()
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TercerPlugin()    
-        ]   
-    }
 }
